f46ae0f615298273de1446b991b5d5f07d44a64b
rpruazol <rpruazol@gmail.com>
2022-08-06 13:08:31 -0400

    error handling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import SearchForm from './SearchForm';
-import Results from './Results'
+import Results from './Results';
 import axios from 'axios';
 
 
@@ -11,7 +11,9 @@ export default class App extends React.Component {
     super(props)
     this.state = {
       city: '',
-      locationObj: null
+      locationObj: null,
+      map: null,
+      error: ''
     }
   }
   saveCity = (value) => {
@@ -21,16 +23,38 @@ export default class App extends React.Component {
 
   queryCity = async (e, city) => {
     e.preventDefault();
-    const response = await axios({
-      method: 'get',
-      url: `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_IQ}&q=${city}&format=json`,
-      responseType: 'stream'
-    })
-      this.setState({ locationObj: response.data[0] })
+    try {
+      const response = await axios({
+        method: 'get',
+        url: `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_IQ}&q=${city}&format=json`,
+        responseType: 'stream'
+      })
+      this.setState({ locationObj: response.data[0] }, () => this.queryMap())
+    } catch (e) {
+      this.setState({ locationObj: null })
+      this.setState({ error: e.message })
+    } finally {
+      console.log('something went wrong.')
+    }
   }
 
+  queryMap = async () => {
+  
+  const mapResponse = await axios({
+    method: 'get',
+    url: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_IQ}&center=${this.state.locationObj.lat},${this.state.locationObj.lon}&zoom=18`,
+    responseType: 'blob'
+  })
+  let reader = new window.FileReader();
+  reader.readAsDataURL(mapResponse.data)
+  reader.onload = () => {
+    console.log(reader.result)
+    this.setState({ map: reader.result })
+  }
+}
+
   render() {
-    console.log(this.state)
+
     return (
       <div className="App">
         <SearchForm
@@ -38,10 +62,12 @@ export default class App extends React.Component {
           queryCity={this.queryCity}
           city={this.state.city}
         />
-        {(this.state.locationObj) &&
+        {(this.state.locationObj) ?
           <Results
             location={this.state.locationObj}
+            map={this.state.map}
           />
+          : <h1>{this.state.error}</h1>
         }
       </div>
     )
