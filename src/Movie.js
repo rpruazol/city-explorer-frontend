e3d7ed7c268667127df86dfaa1e9fafe1ca63014
rpruazol <rpruazol@gmail.com>
2022-08-11 01:58:34 -0400

    carousel

--- src/Movie.js
+++ src/Movie.js
@@ -1,48 +1,50 @@
 import React from "react";
-import ListGroup from "react-bootstrap/ListGroup"
 import Card from 'react-bootstrap/Card'
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Carousel from 'react-bootstrap/Carousel';
+import MovieModal from './MovieModal'
+
 
 export default class Movie extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      selectedIndex: 0
+      selectedIndex: 0,
+      modalShow: false
     }
   }
 
   handleSelect = (selectedIndex, e) => {
-    this.setState({selectedIndex: selectedIndex})
+    this.setState({ selectedIndex: selectedIndex })
   }
+
+  setModalShow = (str, obj) => {
+    this.setState({ modalShow: str}, () => console.log(this.state))
+  }
+
   render() {
     console.log(this.props)
     return (
-      <Row>
-        <Carousel activeIndex={this.state.selectedIndex} onSelect={this.handleSelect}>
-          {(this.props.movieArray.data) &&
-            this.props.movieArray.data.map(obj => {
-              return (
-                  <Carousel.Item>
-                    <Card className="m-auto align-self-center">
-                      <Card.Img variant="top" src={obj.image_url ? `https://www.themoviedb.org/t/p/w220_and_h330_face/${obj.image_url}` : "https://http.cat/404"} />
-                      <Card.Body>
-                        <Card.Title>{obj.title}</Card.Title>
-                      </Card.Body>
-                      <ListGroup className="list-group-flush">
-                        <ListGroup.Item>Overview: {obj.overview} </ListGroup.Item>
-                        <ListGroup.Item>Vote Average: {obj.vote_average} </ListGroup.Item>
-                        <ListGroup.Item>Popularity: {obj.popularity} </ListGroup.Item>
-                        <ListGroup.Item>Released on: {obj.released_on} </ListGroup.Item>
-                      </ListGroup>
-                    </Card>
-                  </Carousel.Item>
-              )
-            })
-          }
-        </Carousel>
-      </Row>
+      <Carousel activeIndex={this.state.selectedIndex} onSelect={this.handleSelect}>
+        {(this.props.movieArray.data) &&
+          this.props.movieArray.data.map((obj, idx) => {
+            return (
+                <Carousel.Item>
+                  <Card className="m-auto align-self-center" >
+                    <Card.Title>{obj.title}</Card.Title>
+                    <Card.Img _id={idx} variant="top" src={obj.image_url ? `https://www.themoviedb.org/t/p/w220_and_h330_face/${obj.image_url}` : "https://http.cat/404"} onClick={() => this.setModalShow(true)}  />
+                  <MovieModal
+                    modalShow={this.state.modalShow}
+                    onHide={() => this.setModalShow(false)}
+                    detail={this.props.movieArray.data[this.state.selectedIndex]}
+                    />
+                  </Card>
+                </Carousel.Item>
+            )
+          })
+        }
+      </Carousel>
     )
   }
 }
--- src/Results.js
+++ src/Results.js
@@ -6,7 +6,7 @@ import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Weather from "./Weather"
 import Movie from "./Movie"
-import CardGroup from "react-bootstrap/CardGroup"
+import Accordion from 'react-bootstrap/Accordion';
 
 
 
@@ -22,22 +22,22 @@ export default class Results extends React.Component {
                 <Card.Title>{this.props.location.display_name}</Card.Title>
               </Card.Body>
               <ListGroup className="list-group-flush">
-              <ListGroup.Item className="b" variant="info">GPS Coordinates</ListGroup.Item>
+                <ListGroup.Item className="b" variant="info">GPS Coordinates</ListGroup.Item>
                 <ListGroup.Item>Lat: {this.props.location.lat}</ListGroup.Item>
                 <ListGroup.Item>Lon: {this.props.location.lon}</ListGroup.Item>
               </ListGroup>
             </Card>
             <Card className="mt-5">
-                  <Weather
-                    forecastArray={this.props.weather}
-                    error={this.props.error}
-                  />
-            </Card>
-            <Card style={{ width: '18rem' }} className="mt-5 m-auto align-self-center">
-              <Movie
-              movieArray={this.props.movies}
+              <Weather
+                forecastArray={this.props.weather}
+                error={this.props.error}
               />
             </Card>
+              <Card style={{ width: '25rem' }} className="mt-5 m-auto mb-5 align-self-center">
+                <Movie
+                  movieArray={this.props.movies}
+                />
+              </Card>
           </Col>
         </Row>
       </Container>
